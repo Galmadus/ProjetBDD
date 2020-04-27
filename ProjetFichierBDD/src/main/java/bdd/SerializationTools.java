5ebb18d392075ff6d0eb2befd39304ba182ef1a0
jerem <jeremy.rasseneur@gmail.com>
2020-04-27 10:05:08 +0200

    Sérialisation classique faite

--- ProjetFichierBDD/src/main/java/bdd/SerializationTools.java
+++ ProjetFichierBDD/src/main/java/bdd/SerializationTools.java
@@ -10,55 +10,77 @@ import java.util.TreeSet;
  * @version 1.0
  */
 class SerializationTools {
-	/**
-	 * Serialise/binarise l'objet passé en paramètre pour retourner un tableau binaire
-	 * @param o l'objet à serialiser
-	 * @return the tableau binaire
-	 * @throws IOException si un problème d'entrée/sortie se produit
-	 */
-	static byte[] serialize(Serializable o) throws IOException {
-		//TODO complete
-		return null;
-	}
+    /**
+     * Serialise/binarise l'objet passé en paramètre pour retourner un tableau binaire
+     *
+     * @param o l'objet à serialiser
+     * @return the tableau binaire
+     * @throws IOException si un problème d'entrée/sortie se produit
+     */
+    static byte[] serialize(Serializable o) throws IOException {
+        if (o != null) {
+            ByteArrayOutputStream tab = new ByteArrayOutputStream();
+            ObjectOutputStream obj = new ObjectOutputStream(tab);
+            obj.writeObject(o);
+            obj.flush();
+            return tab.toByteArray();
+        } else {
+            throw new NullPointerException();
+        }
+    }
 
-	/**
-	 * Désérialise le tableau binaire donné en paramètre pour retrouver l'objet initial avant sa sérialisation
-	 * @param data le tableau binaire
-	 * @return l'objet désérialisé
-	 * @throws IOException si un problème d'entrée/sortie se produit
-	 * @throws ClassNotFoundException si un problème lors de la déserialisation s'est produit
-	 */
-	static Serializable deserialize(byte[] data) throws IOException, ClassNotFoundException {
-		//TODO complete
-		return null;
-	}
+    /**
+     * Désérialise le tableau binaire donné en paramètre pour retrouver l'objet initial avant sa sérialisation
+     *
+     * @param data le tableau binaire
+     * @return l'objet désérialisé
+     * @throws IOException            si un problème d'entrée/sortie se produit
+     * @throws ClassNotFoundException si un problème lors de la déserialisation s'est produit
+     */
+    static Serializable deserialize(byte[] data) throws IOException, ClassNotFoundException {
+        if (data != null) {
+			ByteArrayInputStream tab = new ByteArrayInputStream(data);
+			ObjectInputStream obj = new ObjectInputStream(tab);
+			return (Serializable) obj.readObject();
 
-	/**
-	 * Serialise/binarise le tableau d'espaces libres passé en paramètre pour retourner un tableau binaire, mais selon le schéma suivant :
-	 * Pour chaque interval ;
-	 * <ul>
-	 *     <li>écrire en binaire la position de l'interval</li>
-	 *     <li>écrire en binaire la taille de l'interval</li>
-	 * </ul>
-	 * Utilisation pour cela la classe {@link DataOutputStream}
-	 *
-	 * @param freeSpaceIntervals le tableau d'espaces libres
-	 * @return un tableau binaire
-	 * @throws IOException si un problème d'entrée/sortie se produit
-	 */
-	static byte[] serializeFreeSpaceIntervals(TreeSet<BDD.FreeSpaceInterval> freeSpaceIntervals) throws IOException {
-		//TODO complete
-		return null;
-	}
+		} else {
+            throw new NullPointerException();
+        }
+    }
 
-	/**
-	 * Effectue l'opération inverse de la fonction {@link #serializeFreeSpaceIntervals(TreeSet)}
-	 * @param data le tableau binaire
-	 * @return le tableau d'espaces libres
-	 * @throws IOException si un problème d'entrée/sortie se produit
-	 */
-	static TreeSet<BDD.FreeSpaceInterval> deserializeFreeSpaceIntervals(byte[] data) throws IOException {
-		//TODO complete
-		return null;
-	}
+    /**
+     * Serialise/binarise le tableau d'espaces libres passé en paramètre pour retourner un tableau binaire, mais selon le schéma suivant :
+     * Pour chaque interval ;
+     * <ul>
+     *     <li>écrire en binaire la position de l'interval</li>
+     *     <li>écrire en binaire la taille de l'interval</li>
+     * </ul>
+     * Utilisation pour cela la classe {@link DataOutputStream}
+     *
+     * @param freeSpaceIntervals le tableau d'espaces libres
+     * @return un tableau binaire
+     * @throws IOException si un problème d'entrée/sortie se produit
+     */
+    static byte[] serializeFreeSpaceIntervals(TreeSet<BDD.FreeSpaceInterval> freeSpaceIntervals) throws IOException {
+        if (freeSpaceIntervals != null) {
+            return null;
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    /**
+     * Effectue l'opération inverse de la fonction {@link #serializeFreeSpaceIntervals(TreeSet)}
+     *
+     * @param data le tableau binaire
+     * @return le tableau d'espaces libres
+     * @throws IOException si un problème d'entrée/sortie se produit
+     */
+    static TreeSet<BDD.FreeSpaceInterval> deserializeFreeSpaceIntervals(byte[] data) throws IOException {
+        if (data != null) {
+            return null;
+        } else {
+            throw new NullPointerException();
+        }
+    }
 }
